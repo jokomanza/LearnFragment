31b0a339e68134317e7589b888a8030fecb93403
Joko Supriyanto <Jokomanza@gmail.com>
2021-05-29 17:04:48 +0700

    find existing fragment

--- app/src/main/java/com/joko/learnfragment/MainActivity.kt
+++ app/src/main/java/com/joko/learnfragment/MainActivity.kt
@@ -2,11 +2,15 @@ package com.joko.learnfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +21,18 @@ class MainActivity : AppCompatActivity() {
                 setReorderingAllowed(true)
                 add<ExampleFragment>(R.id.fragment_container_view, args = bundle)
             }
+        }
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            supportFragmentManager.commit {
+                replace<ExampleFragment>(R.id.fragment_container_view, tag = "fragment1", args = bundleOf("some_int" to 666))
+                setReorderingAllowed(true)
+                addToBackStack(null)
+            }
+
+            val fragment: ExampleFragment? =
+                supportFragmentManager.findFragmentByTag("fragment1") as ExampleFragment?
+            Log.i(TAG, "onCreate: $fragment?")
         }
     }
 }
