8e312bc692a03b45d5784f642af733d263c560a5
Joko Supriyanto <Jokomanza@gmail.com>
2021-05-29 17:26:20 +0700

    update fragment

--- app/src/main/java/com/joko/learnfragment/ExampleFragment.kt
+++ app/src/main/java/com/joko/learnfragment/ExampleFragment.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 
-class ExampleFragment : Fragment(R.layout.fragment_example) {
+class ExampleFragment (val repository: ExampleRepository) : Fragment(R.layout.fragment_example) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         val someInt = requireArguments().getInt("some_int")
         Log.i("Fragment", "onViewCreated: $someInt")
