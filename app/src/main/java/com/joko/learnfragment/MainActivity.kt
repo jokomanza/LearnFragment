a5acf9fb4127d2ec2373673a02fe9326ab2894c7
Joko Supriyanto <Jokomanza@gmail.com>
2021-05-29 19:00:53 +0700

    update MainActivity.kt

--- app/src/main/java/com/joko/learnfragment/MainActivity.kt
+++ app/src/main/java/com/joko/learnfragment/MainActivity.kt
@@ -2,9 +2,12 @@ package com.joko.learnfragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.core.os.bundleOf
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +19,19 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.commit {
                 setReorderingAllowed(true)
                 add<ExampleFragment>(R.id.fragment_container_view, args = bundle)
+            }
+        }
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            supportFragmentManager.commit {
+                setCustomAnimations(
+                    R.anim.slide_in,
+                    R.anim.fade_out,
+                    R.anim.fade_in,
+                    R.anim.slide_out
+                )
+                replace<Example2Fragment>(R.id.fragment_container_view)
+                addToBackStack(null)
             }
         }
     }
